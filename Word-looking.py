b89b5ae1b4dfc9e71481bfbceeba0428ca376ca3
JonasL <460363851@qq.com>
2017-07-29 11:25:06 +0800

    Create Word-looking.py

---
+++ Word-looking.py
@@ -0,0 +1,11 @@
+import json
+
+with open("Lib.txt" , "rt" ,encoding = "UTF-8") as file :
+	word = eval(file.read())
+print('词库初始化完毕！请输入单词')
+while 1 :
+	w = input()
+	if w in word.keys() :
+		print(word[w])
+	else :
+		print('未找到此单词！')
